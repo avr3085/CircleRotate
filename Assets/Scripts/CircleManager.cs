1e97f7e8593e60347c048b5add43143359c44a87
avr3085 <avr3085@gmail.com>
2022-04-17 11:14:53 +0530

    first commit

---
+++ Assets/Scripts/CircleManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class CircleManager : MonoBehaviour
+    {
+        public GameObject circle, otherCircle;
+        [Range(0, 360)] public float degree;
+        [Range(0, 359)] public float otherDegree;
+        [Range(2,10)] public float radius;
+
+        private void Update()
+        {
+            var x = radius * Mathf.Cos(degree *  Mathf.Deg2Rad);
+            var y = radius * Mathf.Sin(degree * Mathf.Deg2Rad);
+            var otherX = radius * Mathf.Cos((degree + otherDegree) * Mathf.Deg2Rad);
+            var otherY = radius * Mathf.Sin((degree + otherDegree) * Mathf.Deg2Rad);
+            circle.transform.position = new Vector3(x, y, 0f);
+            otherCircle.transform.position = new Vector3(otherX, otherY, 0f);
+        }
+    }
+}
